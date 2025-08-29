94189d3816f0897c9680ffc6f0807fc0542097b7
Vincent Paul Boda <bodavincentpaul@gmail.com>
2025-08-29 13:50:58 +0000

    csn u add space background in motion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import { Background } from '@/components/ui/background';
 
 export const metadata: Metadata = {
   title: 'FinTrack',
@@ -24,10 +25,7 @@ export default function RootLayout({
         />
       </head>
       <body className="font-body antialiased relative">
-        <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-gradient-radial"></div>
-        <div className="absolute bottom-0 left-0 right-0 top-0 -z-10 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div>
-        <div className="absolute -bottom-40 -left-20 -z-10 h-80 w-80 rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-        <div className="absolute -right-40 -top-40 -z-10 h-80 w-80 float rounded-full bg-[radial-gradient(circle_farthest-side,rgba(0,128,255,.15),rgba(255,255,255,0))]"></div>
+        <Background />
         {children}
         <Toaster />
       </body>
---
+++ src/components/ui/background.tsx
@@ -0,0 +1,51 @@
+export function Background() {
+  return (
+    <div className="absolute inset-0 -z-10 h-full w-full bg-background">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+      <div className="absolute -bottom-40 -left-20 h-80 w-80 rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+      <div className="absolute -right-40 -top-40 h-80 w-80 rounded-full bg-[radial-gradient(circle_farthest-side,rgba(0,128,255,.15),rgba(255,255,255,0))]"></div>
+      <div
+        className="absolute inset-0 h-full w-full bg-cover bg-center"
+        style={{
+          backgroundImage:
+            'url(https://assets.aceternity.com/textures/stars.png)',
+          animation: 'animate-stars 240s linear infinite',
+        }}
+      ></div>
+      <div className="absolute inset-0 h-full w-full">
+        <div
+          className="absolute h-full w-full bg-repeat"
+          style={{
+            backgroundImage:
+              'url(https://assets.aceternity.com/textures/grid.png)',
+          }}
+        ></div>
+      </div>
+      <div className="pointer-events-none absolute inset-0 z-10 h-full w-full [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="absolute inset-0 z-20 overflow-hidden">
+        <div className="absolute bottom-0 left-1/2 h-4/5 w-1/2 -translate-x-1/2 rounded-t-full bg-gradient-to-t from-background via-transparent to-transparent"></div>
+        {[...Array(2)].map((_, i) => (
+          <div
+            key={`shooting-star-${i}`}
+            className="absolute h-0.5 w-0.5 rounded-full bg-primary shadow-[0_0_0_1px_#ffffff10]"
+            style={{
+              top: `${Math.random() * 100}%`,
+              left: `${Math.random() * 100}%`,
+              animation: `animate-shooting-star ${
+                Math.random() * 8 + 4
+              }s linear infinite`,
+              animationDelay: `${Math.random() * 5}s`,
+            }}
+          >
+            <div
+              className="absolute top-1/2 -z-10 h-[1px] w-[50px] -translate-y-1/2 bg-gradient-to-r from-primary to-transparent"
+              style={{
+                transform: 'rotate(-45deg)',
+              }}
+            ></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
