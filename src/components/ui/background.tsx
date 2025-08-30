ac326d53986add2c54ee4e08fb1647dbece3913d
Vincent Paul Boda <bodavincentpaul@gmail.com>
2025-08-30 13:49:12 +0000

    undo the changes remain the old changes only

--- src/components/ui/background.tsx
+++ src/components/ui/background.tsx
@@ -1,8 +1,8 @@
 export function Background() {
   return (
     <div className="fixed inset-0 -z-10 h-full w-full bg-background">
-      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,rgba(100,100,100,0.1)_1px,transparent_1px),linear-gradient(to_bottom,rgba(100,100,100,0.1)_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
-      <div className="absolute inset-0 -z-10 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e8f,transparent)]"></div>
+      <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
+      <div className="absolute inset-0 -z-10 bg-[radial-gradient(circle_500px_at_50%_200px,hsl(var(--primary)/0.15),transparent)]"></div>
     </div>
   );
 }
