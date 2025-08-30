71cf58377aafbe680614d3b1b789eaa95fd7f645
Vincent Paul Boda <bodavincentpaul@gmail.com>
2025-08-30 13:38:22 +0000

    the grids line still in the background remove this i want clear and plai

--- src/components/ui/background.tsx
+++ src/components/ui/background.tsx
@@ -1,7 +1,6 @@
 export function Background() {
   return (
     <div className="absolute inset-0 -z-10 h-full w-full bg-background">
-      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div>
       <div className="absolute -bottom-40 -left-20 h-80 w-80 rounded-full bg-[radial-gradient(circle_farthest-side,rgba(138,43,226,.15),rgba(255,255,255,0))]"></div>
       <div className="absolute -right-40 -top-40 h-80 w-80 rounded-full bg-[radial-gradient(circle_farthest-side,rgba(100,20,255,.15),rgba(255,255,255,0))]"></div>
       <div
@@ -12,15 +11,6 @@ export function Background() {
           animation: 'animate-stars 240s linear infinite',
         }}
       ></div>
-      <div className="absolute inset-0 h-full w-full">
-        <div
-          className="absolute h-full w-full bg-repeat"
-          style={{
-            backgroundImage:
-              'url(https://assets.aceternity.com/textures/grid.png)',
-          }}
-        ></div>
-      </div>
       <div className="pointer-events-none absolute inset-0 z-10 h-full w-full [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="absolute inset-0 z-20 overflow-hidden">
         <div className="absolute bottom-0 left-1/2 h-4/5 w-1/2 -translate-x-1/2 rounded-t-full bg-gradient-to-t from-background via-transparent to-transparent"></div>
